8b754c7d303d70fa2908e5746d2ea1c85961c399
MakotoOba <mac.kageyama@gmail.com>
2022-10-01 11:48:47 +0900

    Change Background Color の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends HookWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Change Button',
       theme: ThemeData(
-        // TODO: 背景色を変数に入れて可変にする
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyAppPage(title: 'Change Background Color'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyAppPage extends HookWidget {
+  const MyAppPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-// TODO: Provider やHooksを使いstateの管理を行う
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // TODO: change color Buttonを作成する
-    // TODO: change color Button を押した時に背景の色が変化する 赤→青→緑
-    // TODO: button の文字を切り抜きにするか、透明にする
+    //インスタンスを作成
+    final change = useState(Colors.green);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        backgroundColor: change.value,
+        body: Center(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'ボタンを押すと背景の色が変わります',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (change.value == Colors.green) {
+                    change.value = Colors.red;
+                  } else if (change.value == Colors.red) {
+                    change.value = Colors.blue;
+                  } else {
+                    change.value = Colors.green;
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: change.value,
+                  backgroundColor: Colors.white,
+                ),
+                child: const Text(
+                  "Change Color",
+                ),
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        )));
   }
 }
-
-//どんな仕組みだ　ボタンを押したらボタンイベントが発火して
-//イベントの関数が背景の色を帰る処理が実行される
-//元々、stateに背景色の値を保持しておいて、プロバイダーかhooksでstateの変更を行う
-//おそらくHooks だけで状態管理できそう　１ページで収まるから
